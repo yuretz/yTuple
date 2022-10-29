3558aea67f4b55d0c75ab50d081a45d479a2bf90
yk <yuretz@gmail.com>
2022-10-29 23:54:26 +0200

    Added tuple to enumerable extension

---
+++ src/Tupleth/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace Tupleth;
+
+internal static class Extensions
+{
+    public static IEnumerable<object?> ToEnumerable(this ITuple tuple)
+    {
+        for(var i = 0; i < tuple.Length; i++)
+        {
+            yield return tuple[i] is ITuple nested ? nested.ToEnumerable() : tuple[i];
+        }
+    }
+}
