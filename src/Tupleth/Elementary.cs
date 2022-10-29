6fae6f7de1e2084e25e7602b0cc0519f5992a657
yk <yuretz@gmail.com>
2022-10-29 23:53:45 +0200

    Added elementary operation helpers

---
+++ src/Tupleth/Elementary.cs
@@ -0,0 +1,25 @@
+﻿namespace Tupleth;
+
+public static class Elementary
+{
+    public static readonly object? nil = null;
+
+    public const string atom = "atom";
+    public const string car = "car";
+    public const string cdr = "cdr";
+    public const string cons = "cons";
+    public const string quote = "quote";
+    public const string cond = "cond";
+    public const string lambda = "lambda";
+    public const string label = "label";
+
+    public const string eq = "==";
+    public const string and = "&&";
+    public const string or = "||";
+    public const string not = "!";
+
+    public const string plus = "+";
+    public const string minus = "-";
+    public const string mul = "*";
+    public const string div = "/";
+}
