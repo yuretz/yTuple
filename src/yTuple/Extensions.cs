9ebc3bdcaa434f2ca699a4d6cf8bad51b23a2b64
yuretz <yuretz@gmail.com>
2022-11-09 10:50:21 +0100

    enumerable ToTuple() extension method

--- src/yTuple/Extensions.cs
+++ src/yTuple/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace yTuple;
@@ -19,4 +20,44 @@ public static class Extensions
         }
     }
 
+    public static ITuple ToTuple(this IEnumerable<object?> enumerable, bool recurse) =>
+        ToTuple(enumerable.GetEnumerator(), recurse);
+
+    public static ITuple ToTuple(IEnumerator<object?> enumerator, bool recurse)
+    {
+        var items = new List<object?>();
+
+        while(items.Count < 7 && enumerator.MoveNext())
+        {
+            items.Add(enumerator.Current);
+        }
+
+        if(items.Count == 7)
+        {
+            var rest = ToTuple(enumerator, recurse);
+            if(rest.Length != 0)
+            {
+                items.Add(rest);
+            }
+        }
+
+        if(items.Count == 0)
+        {
+            return default(ValueTuple);
+        }
+
+        var types = items.Select(item => item?.GetType() ?? typeof(object)).ToArray();
+        return (ITuple)_types[items.Count - 1].MakeGenericType(types).GetConstructor(types)!.Invoke(items.ToArray());
+    }
+
+    private static readonly Type[] _types = new Type 
+                              [8]
+    {typeof(ValueTuple        < >        ),
+     typeof(ValueTuple       < , >       ),
+     typeof(ValueTuple      < , , >      ),
+     typeof(ValueTuple     < , , , >     ),
+     typeof(ValueTuple    < , , , , >    ),
+     typeof(ValueTuple   < , , , , , >   ),
+     typeof(ValueTuple  < , , , , , , >  ),
+     typeof(ValueTuple < , , , , , , , > )};
 }
